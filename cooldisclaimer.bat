086ccd846770664084f92b82460f7e38c5844d80
Niilas Oikarainen <noikarainen@yahoo.com>
2020-07-26 15:56:10 +0300

    Add files via upload

---
+++ cooldisclaimer.bat
@@ -0,0 +1,41 @@
+@echo off
+title DISCLAIMER
+echo This program is a joke program.
+echo It doesn't make any changes to your device.
+echo If you prefer to run the program, wait 5 seconds and press any key on your computer.
+echo If you don't prefer to run the program, please click the X in the up-right corner.
+timeout 1 >nul
+cls
+echo This program is a joke program.
+echo It doesn't make any changes to your device.
+echo If you prefer to run the program, wait 4 seconds and press any key on your computer.
+echo If you don't prefer to run the program, please click the X in the up-right corner.
+timeout 1 >nul
+cls
+echo This program is a joke program.
+echo It doesn't make any changes to your device.
+echo If you prefer to run the program, wait 3 seconds and press any key on your computer.
+echo If you don't prefer to run the program, please click the X in the up-right corner.
+timeout 1 >nul
+cls
+echo This program is a joke program.
+echo It doesn't make any changes to your device.
+echo If you prefer to run the program, wait 2 seconds and press any key on your computer.
+echo If you don't prefer to run the program, please click the X in the up-right corner.
+timeout 1 >nul
+cls
+echo This program is a joke program.
+echo It doesn't make any changes to your device.
+echo If you prefer to run the program, wait 1 seconds and press any key on your computer.
+echo If you don't prefer to run the program, please click the X in the up-right corner.
+timeout 1 >nul
+cls
+echo Press any key to continue.
+echo You are still able to close the program.
+pause >nul
+cls
+title Hi!
+:a
+echo Hi!
+echo _____________________________________________________________________________
+goto a
